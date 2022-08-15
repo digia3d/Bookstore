fcf69df2e6536bce6db7885b72762dd9ade85500
Aleksandra Ujvari <ujvari65@gmail.com>
2022-08-15 16:15:55 +0200

    Add redux books.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import Store from './redux/configureStore';
+import './App.css';
 import Header from './components/Header';
 import Books from './components/pages/Books';
 import Categories from './components/pages/Categories';
-import './App.css';
 
 function App() {
   return (
+
     <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Books />} />
-        <Route exact path="Books" element={<Books />} />
-        <Route path="Categories" element={<Categories />} />
-      </Routes>
+      <Provider store={Store}>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Books />} />
+          <Route exact path="Books" element={<Books />} />
+          <Route path="Categories" element={<Categories />} />
+        </Routes>
+      </Provider>
     </Router>
   );
 }

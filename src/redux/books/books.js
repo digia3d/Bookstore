bb7e5795586a937c342e059badc32cc80d363479
Aleksandra Ujvari <ujvari65@gmail.com>
2022-08-17 12:54:17 +0200

    Add books in books.js and reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,19 +1,54 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [{
+  id: uuidv4,
+  title: 'The Hunger Games',
+  author: 'Suzanne Collins',
+  category: 'Action',
+  progress: 64,
+  currentChapter: 17,
+},
+{
+  id: uuidv4,
+  title: 'Dune',
+  author: 'Frank Herbert',
+  category: 'Science Fiction',
+  progress: 8,
+  currentChapter: 3,
+},
+{
+  id: uuidv4,
+  title: 'Capital in the Twenty-First century',
+  author: 'Suzanne Collins',
+  category: 'Economy',
+  progress: 0,
+  currentChapter: 'introduction',
+},
+];
 
-export function addBook(payload) {
+export function addBook(newBook) {
   return {
     type: ADD_BOOK,
-    payload,
+    payload: {
+      id: uuidv4,
+      title: newBook.title,
+      author: newBook.author,
+      category: newBook.category,
+      progress: 0,
+      currentChapter: 0,
+    },
   };
 }
 
-export function removeBook(payload) {
+export function removeBook(id) {
   return {
     type: REMOVE_BOOK,
-    payload,
+    payload: {
+      bookid: id,
+    },
   };
 }
 
